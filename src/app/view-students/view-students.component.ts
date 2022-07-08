c37bc219ebcefb7b38dfe8e51db70db39d2206e9
Jayavi11 <jayavinayakjayakumar@gmail.com>
2022-07-08 12:19:36 +0530

    new1

--- src/app/view-students/view-students.component.ts
+++ src/app/view-students/view-students.component.ts
@@ -11,5 +11,13 @@ export class ViewStudentsComponent implements OnInit {
 
   ngOnInit(): void {
   }
+name=
+[
+{"Name":"Rahul","Age":45,"Photo":"https://img.freepik.com/free-photo/half-profile-image-handsome-young-caucasian-man-with-good-skin-brown-eyes-black-stylish-hair-stubble-posing-isolated-against-blank-wall-looking-front-him-smiling_343059-4560.jpg?w=2000"},
+{"Name":"Reji","Age":50,"Photo":"https://loveshayariimages.in/wp-content/uploads/2021/10/1080p-Latest-Whatsapp-Profile-Images-1.jpg"},
+{"Name":"Manu","Age":25,"Photo":"https://images.unsplash.com/photo-1628563694622-5a76957fd09c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8aW5zdGFncmFtJTIwcHJvZmlsZXxlbnwwfHwwfHw%3D&w=1000&q=80"},
+{"Name":"Biju","Age":40,"Photo":"https://www.kapwing.com/resources/content/images/2021/04/tiktok-profile-picture-idea-4--1--1.jpeg"}
 
+
+]
 }
